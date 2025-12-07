6c00e20c37eb5bfd3192a6b127c0eca9e63e09c1
ASHIRWAD KUMAR JHA <101699746+Ashirwad2004@users.noreply.github.com>
2025-12-07 16:42:13 +0530

    array_most_impqueestion

---
+++ variable.js
@@ -0,0 +1,29 @@
+//const accounid = 12345
+//let accountemail = "satyamjha@gmail.com"
+//var accountpassword = "satyam@123"
+//accountcity = "Bangalore"  // global variable
+//let AccountState;
+//console.table([accounid, accountemail, accountpassword, accountcity]);
+//account id =2
+//const , let, var
+//console.log(2%6);
+//console.log("2"===2);
+
+
+
+// const id = Symbol("1234");
+// const anotherId=Symbol("1234");
+// console.log(id==anotherId);
+
+// 
+
+let user1 = {
+    email: "ashirwad@gmail.com",
+    upi: "ashirwad@oksbi"
+
+}
+
+let user2 = user1;
+user2.email = "satyam@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
