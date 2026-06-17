277acec0fa7f1bc0abc862096f9db0f4716f3016
ASHIRWAD KUMAR JHA <101699746+Ashirwad2004@users.noreply.github.com>
2026-06-17 14:02:37 +0530

    array push

---
+++ loops.js
@@ -0,0 +1,41 @@
+//foor loops are used to repeat a block of code a certain number of times or until a certain condition is met.
+//print 10 times names
+
+let i =10;
+for(let i=0; i<10; i++){
+    console.log("ashirwad");
+}
+
+//print 1 to 5 
+
+for(let i=1; i<=5; i++){
+    console.log(i);
+}
+
+//print 5 to 1
+
+for(let i=5; i>=1; i--){
+    console.log(i);
+}
+
+//while loop is used to repeat a block of code as long as a certain condition is true.
+
+//print 1 to 5  
+let j=1;
+while(j<=5){
+    console.log(j);
+    j++;
+}
+
+//print 5 to 1
+let k=5;  
+while(k>=1){
+    console.log(k);
+    k--;
+}    
+//print 5 to 10
+let s =5;
+while (s>=10){
+    console.log(s)
+    s++
+}
